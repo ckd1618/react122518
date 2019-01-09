76df0e3715aa11fb52366b2cce4ef87e0c81a05d
ckd1618 <ckd1618@gmail.com>
2019-01-08 19:43:40 -0800

    added state and setState

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Same thing but the function version instead of the class version
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         position => console.log(position),
+//         err => console.log(err)
+//     )
+//     return <div>Hi There!</div>;
+// };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null, errorMessage: null};
+
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ lat: position.coords.latitude })
+            },
+            err => this.setState({errorMessage: err.message})
+        );
+    }
+
+    render(){
+
+        if (this.state.lat) {
+            return <div>Latitude: {this.state.lat}</div>;
+        }else if (this.state.errorMessage){
+            return <div>Error: {this.state.errorMessage} </div>;
+        }
+        return <div>Loading...</div>;
+
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('#root'));
