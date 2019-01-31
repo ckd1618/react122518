8ca04ede1ff4f4990aab73563059fb3003fe012f
ckd1618 <ckd1618@gmail.com>
2019-01-31 14:03:29 -0800

    update

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
-const App = () => {
-    return <div>Damnit</div>;
-};
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar onSubmit={ this.onSearchSubmit } />
+            </div>
+        );
+    }
+}
 
 export default App;
