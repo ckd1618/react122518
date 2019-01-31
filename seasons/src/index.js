de53202121a1db23edb49f80aaab2c87a7952881
ckd1618 <ckd1618@gmail.com>
2019-01-30 18:41:47 -0800

    added loading screen

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 
 //Same thing but the function version instead of the class version
 // const App = () => {
@@ -21,16 +22,20 @@ class App extends React.Component {
         );
     }
 
-    render(){
-
+    renderContent() {
         if (this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />;
             //return <div>Latitude: {this.state.lat}</div>;
         }else if (this.state.errorMessage){
             return <div>Error: {this.state.errorMessage} </div>;
         }
-        return <div>Loading...</div>;
+        return <Spinner message="Please accept location request" />;
     }
+
+    render() {
+        return this.renderContent();
+    }
+    
 }
 
 ReactDom.render(<App />, document.querySelector('#root'));
