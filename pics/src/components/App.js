13404298a0f48850d64bcc41d04a8d1dde2bd4d0
ckd1618 <ckd1618@gmail.com>
2019-01-31 12:09:15 -0800

    rectified changes to seasons file made by accident

---
+++ pics/src/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => {
+    return <div>Damnit</div>;
+};
+
+export default App;
--- seasons/src/Spinner.js
+++ seasons/src/Spinner.js
@@ -3,13 +3,14 @@ import React from 'react';
 const Spinner = (props) => {
     return (
         <div class="ui active dimmer">
-            <div class="ui big text loader">{ props.message || 'Loading' }</div>
+            <div class="ui big text loader">{ props.message }</div>
+            {/* <div class="ui big text loader">{ props.message || 'Loading' }</div> */}
         </div>
     );
 }
 
-// Spinner.defaultProps = {
-//     message: 'Loading'
-// }
+Spinner.defaultProps = {
+    message: 'Loading'
+}
 
 export default Spinner;
