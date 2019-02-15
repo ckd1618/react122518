a79356e263466bd3d08597ea46f87c939f707b08
ckd1618 <ckd1618@gmail.com>
2019-02-14 22:03:49 -0800

    half way done, created all components, need more code to make videos appear

---
+++ videos/src/components/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+
+class App extends React.Component {
+    state = { videos: [] };
+
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: { q: term }
+        });
+        this.setState({videos: response.data.items});
+        console.log(this.state.videos);
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <div>I have {this.state.videos.length} videos</div>
+                <VideoList videos={this.state.videos} />
+            </div>
+            
+        )
+    }
+}
+
+export default App;
